e6a32d6590747e467286fcdebac1070268bc2c2e
taerinlee <taerin.lee@inhousesoft.com>
2026-02-27 03:54:15 +0900

    feat: api path 추가

--- src/api/docClassification/DocClassificationApiPaths.ts
+++ src/api/docClassification/DocClassificationApiPaths.ts
@@ -1,35 +1,26 @@
 /**
- * 목록 조회
+ * 문서분류
  */
-export const selectDocClassificationListApiPath = () =>
-  "/api/dr/documentclassification/search";
+// 목록 조회
+export const selectDocClassificationListApiPath = () => "/api/dr/documentclassification/search";
 
-/**
- * 상세 조회
- */
-export const selectDocClassificationDetailApiPath = (docClsfNo: string) =>
-  `/api/dr/documentclassification/${docClsfNo}`;
+// 상세 조회
+export const selectDocClassificationDetailApiPath = (docClsfNo: string) => `/api/dr/documentclassification/${docClsfNo}`;
 
-/**
- * 히스토리 조회
- */
-export const selectDocClassificationHistoryApiPath = (docClsfNo: string) =>
-  `/api/dr/documentclassification/${docClsfNo}/history`;
+// 하위분류 조회
+export const selectocClassificationDChidrenApiPath = (eldocNo: string) => `/api/dr/documentclassification/${eldocNo}/chidren`;
 
-/**
- * 등록
- */
-export const insertDocClassificationApiPath = () =>
-  "/api/dr/documentclassification/add";
+// 이력 조회
+export const selectDocClassificationHistoryApiPath = (docClsfNo: string) => `/api/dr/documentclassification/${docClsfNo}/history`;
 
-/**
- * 수정
- */
-export const updateDocClassificationApiPath = () =>
-  "/api/dr/documentclassification/update";
+// 등록
+export const insertDocClassificationApiPath = () => "/api/dr/documentclassification/add";
 
-/**
- * 삭제
- */
-export const deleteDocClassificationApiPath = (docClsfNo: string) =>
-  `/api/dr/documentclassification/${docClsfNo}/delete`;
+// 수정
+export const updateDocClassificationApiPath = () => "/api/dr/documentclassification/update";
+
+// 삭제
+export const deleteDocClassificationApiPath = (docClsfNo: string) => `/api/dr/documentclassification/${docClsfNo}/delete`;
+
+// 문서분류 사용안함
+export const addDocClassificationUnuseApiPath = (docClsfNo: string) => `/api/dr/documentclassification/${docClsfNo}/unuse`;
--- src/api/docDestruction/DocDestructionApiPaths.ts
+++ src/api/docDestruction/DocDestructionApiPaths.ts
@@ -1,30 +1,33 @@
 /**
- * 목록 조회
+ * 파기문서
  */
-export const selectDocDestructionListApiPath = () =>
-  "/api/dr/docDestruction/search";
+// 파기문서 목록 조회
+export const selectDocDestructionListApiPath = () => "/api/dr/docDestruction/search";
+
+// 상세 조회
+export const selectDocDestructionDetailApiPath = (eldocNo: string) => `/api/dr/docDestruction/${eldocNo}`;
+
+// 문서이력 조회
+export const selectDocDestructionHistoryApiPath = (eldocNo: string) => `/api/dr/docDestruction/${eldocNo}/history`;
+
+// 문서파기 신청 및 승인
+export const updateDocDestructionApiPath = () => "/api/dr/docDestruction/docPrcsPrstUpdate";
+
+// 전자문서 보유기간 일괄 수정
+export const updateDocDestructionAllApiPath = () => "/api/dr/docDestruction/updatehldprdall";
+
 
 /**
- * 상세 조회
+ * 공람
  */
-// TODO: Detail data view logic
-export const selectDocDestructionDetailApiPath = (eldocNo: string) =>
-  `/api/dr/docDestruction/${eldocNo}`;
+// 공람 목록 조회
+export const selectDocDestructionInqauthrtListApiPath = (eldocNo: string) => `/api/dr/docDestruction/${eldocNo}/inqauthrt`;
 
-/**
- * 등록
- */
-// TODO: create logic
-export const insertDocDestructionApiPath = () => "/api/dr/docDestruction/add";
+// 공람 등록
+export const insertDocDestructionInqauthrtApiPath = (eldocNo: string) => `/api/dr/docDestruction/${eldocNo}/inqauthrt`;
 
-/**
- * 문서파기 신청, 승인
- */
-export const updateDocDestructionApiPath = () =>
-  "/api/dr/docDestruction/docPrcsPrstUpdate";
+// 공람 삭제
+export const deleteDocDestructionInqauthrtApiPath = (eldocNo: string, inqAuthrtNo: string) => `/api/dr/docDestruction/${eldocNo}/inqauthrt/${inqAuthrtNo}/soft-delete`;
 
-/**
- * 프린트 데이터 조회
- */
-export const selectDocDestructionPrvcIncl = () =>
-  "/api/dr/docDestruction/search";
+// 공람 이력 조회
+export const selectDocDestructionInqauthrtHistoryApiPath = (eldocNo: string) => `/api/dr/docDestruction/${eldocNo}/inqauthrt/history`;
