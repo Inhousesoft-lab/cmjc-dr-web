fcb2b229d026038fda0a1166722902ea35a39b0f
taerinlee <taerin.lee@inhousesoft.com>
2026-03-04 01:34:18 +0900

    feat: 보유기간관리 reducer 추가

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -2,6 +2,7 @@ import { combineReducers } from "@reduxjs/toolkit";
 import docClsfReducer from "@/features/clsf/DocClsfSlice";
 import docClassificationListReducer from "@/features/classification/DocClassificationListSlice";
 import digitalDocListReducer from "@/features/digitalDoc/DigitalDocSlice";
+import holdingInstitutionListReducer from "@/features/holdingInstitution/HoldingInstitutionSlice";
 import menuReducer from "@/features/auth/MenuSlice";
 import uiReducer from "@/features/ui/uiSlice";
 
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
   docClsf: docClsfReducer,
   docClassificationList: docClassificationListReducer,
   digitalDocList: digitalDocListReducer,
+  holdingInstitutionList: holdingInstitutionListReducer,
   menuList: menuReducer,
   ui: uiReducer,
 });
